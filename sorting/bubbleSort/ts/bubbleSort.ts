1ce46db03dd58f51a6f0bb0a3b8edb8856da6a4a
Tommy Zarick <zarick.6@osu.edu>
2021-01-12 23:02:24 -0500

    index inc bug fix

--- sorting/bubbleSort/ts/bubbleSort.ts
+++ sorting/bubbleSort/ts/bubbleSort.ts
@@ -1,7 +1,7 @@
 /* Bubble Sort */
 export const sort = (array: number[]): number[] => {
   let swapsPerSweep = 0;
-  for (let i = array.length - 1; i >= 0; i++) {
+  for (let i = array.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
       if (array[j] > array[j + 1]) {
         noTempSwap(array, j, j + 1);
